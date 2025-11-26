017fa4406e3bf86ceef3b503b89a5ded894b6365
Joshua-5665 <joshuac726@nycstudents.net>
2025-11-26 14:02:25 -0500

    dvv

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -18,10 +18,28 @@ const shelf = [
   {name:"Book",
    img: "https://m.media-amazon.com/images/I/81xVnX8OodL._SY385_.jpg"
   },
-  {name:"Book"},
-  {name:"Book"},
+  {name:"Book",
+   img: "https://m.media-amazon.com/images/I/51CbDLV85oL._SY445_SX342_FMwebp_.jpg"
+  },
+  {name:"Book",
+   img: "https://m.media-amazon.com/images/I/81B8uLQ-nFL._SY466_.jpg"
+  },
   {name:"Book"}
 ];
+
+    const container = document.querySelector(".main2");
+    container.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="card">
+            <img class="card-img" src="${books.img}"/>
+            <h5 class="card-Header">${books.name}</h5>
+            <h6 class="card-price">$${books.price}</h6>
+            <button class="add-to-cart-btn">ADD TO CART</button>
+        </div>`
+    );
+
+  
+
 
 
 
