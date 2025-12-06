22449ee4a45b150bd3a2df396fb73a474c8bbf81
Joshua-5665 <joshuac726@nycstudents.net>
2025-12-05 21:02:58 -0500

    eded

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -14,7 +14,7 @@ document.querySelector(".toggleColor").addEventListener("click", function () {
 
 
 
-const shelf = [
+const recommended = [
   {
    img: "https://m.media-amazon.com/images/I/81xVnX8OodL._SY385_.jpg"
   },
@@ -28,38 +28,101 @@ const shelf = [
    img: "https://m.media-amazon.com/images/I/51shYHwY06L._SY445_SX342_FMwebp_.jpg"
   },
   {
-   img: "https://m.media-amazon.com/images/I/51SJCNqJ4jL._SX342_SY445_FMwebp_.jpg"
-  },
-  {
    img: "https://www.beacon.org/Assets/ProductImages/978-080706010-0.jpg" 
   },
   {
     img: "https://m.media-amazon.com/images/I/81G3FEapceL._SY466_.jpg"
-  }
+  },
 ];
-  function inject(shelf) {
+  function inject(recommended) {
     const container = document.querySelector(".main2");
     container.insertAdjacentHTML(
         "afterbegin",
         `<div class="main2">
-            <img class="card-img" src="${shelf.img}"/>
-            <button class="add-to-cart-btn">ADD TO CART</button>
+            <img class="card-img" src="${recommended.img}"/>
+            <button class="add-to-cart-btn">ADD TO LIBRARY</button>
         </div>`
     );
   };
-    function filterBooks(type) {
-    let filterByType = [];
-    if (type === "All") {
-        filterByType = shelf;
-    } else {
-        filterByType = shelf.filter((shelf) => shelf.type === type);
-    }
-    const container = document.querySelector(".main2");
-    container.innerHTML = "";
-    filterByType.forEach(inject);
-  }
-shelf.forEach(inject);
+recommended.forEach(inject);
+
+const read = [
+  {
+    img: "https://m.media-amazon.com/images/I/41dODnI1g8L.jpg",
+    libraryBook : false
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/51ohFqjH4lL._SY445_SX342_FMwebp_.jpg",
+    libraryBook : false
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/81uTSdZJCZL._SY522_.jpg",
+    libraryBook : false
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/818rZeonl4L._SY522_.jpg",
+    libraryBook : false
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/81rfZ1V2IpL._SY522_.jpg",
+    libraryBook : false
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/51XQktN9oeL._SY445_SX342_FMwebp_.jpg",
+    libraryBook : false
+  },
+];
+    function inject2(read) {
+    const container2 = document.querySelector(".main3");
+    container2.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="main3">
+            <img class="card-img2" src="${read.img}"/>
+            <button class="add-to-cart-btn2">ADD TO LIBRARY</button>
+        </div>`
+    );
+  };
+read.forEach(inject2);
   
+
+const buttons = document.querySelectorAll(".add-to-cart-btn2")
+buttons.forEach((btn) => {
+  btn.addEventListener("mouseover", () => {
+    btn.style.backgroundColor = "rgb(219, 161, 0)";
+  });
+
+  btn.addEventListener("mouseout", () => {
+    btn.style.backgroundColor = "";
+  });
+  btn.addEventListener("click", ()=> {
+    window.alert("Book Added to Library!")
+    libraryBook = true;
+  });
+});
+const buttons2 = document.querySelectorAll(".add-to-cart-btn2")
+buttons2.forEach((btn) => {
+  btn.addEventListener("mouseover", () => {
+    btn.style.backgroundColor = "rgb(219, 161, 0)";
+  });
+
+  btn.addEventListener("mouseout", () => {
+    btn.style.backgroundColor = "";
+  });
+  btn.addEventListener("click", ()=> {
+    window.alert("Book Added to Library!")
+  });
+});
+
+const search = document.querySelector("h1")
+  search.addEventListener("click", ()=> {
+    window.alert("The Search Bar is Currently Unavailable. Please Try Again Later.")
+  })
+
+const home = document.querySelector(".home");
+home.addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/vite-project/src/index2.html";
+});
+
 
 
 
